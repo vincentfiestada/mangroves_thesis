c953699a0bd033c97f44bf1dfd32219c43306e64
Vincent Fiestada <vffiestada@up.edu.ph>
2017-01-29 21:02:19 +0800

    Add White Noise term and update function to Patch

--- software/Patch.py
+++ software/Patch.py
@@ -1,10 +1,14 @@
 # Patch represents a patch or cell in the world grid. It contains environmental data
+import numpy as np
+
 class Patch:
 	def __init__(self, s, i, c, g):
 		self.salinity = s ## Salinity effect here
 		self.inundation = i ## Inundation effect here
 		self.competition = c ## Competition effect here
 		self.growable = g ## Whether plants can be planted here or not
+		self.chanceOfBirth = 0.0 ## Chance that a seedling will grow in this patch
+		self.whiteNoise = 0.0 ## Last computed white noise term for this patch
 	def getSalinity(self):
 		return self.salinity
 	def setSalinity(self, value):
@@ -17,6 +21,13 @@ class Patch:
 		return self.competition
 	def setCompetition(self, value):
 		self.competition = value
+	def getWhiteNoise(self):
+		return self.whiteNoise
+	def updateWhiteNoise(self, deltaT):
+		## Update white noise term
+		##  B(0) = 0
+		##  B(t) = B(t-1) + Norm(mean = 0, std deviation = deltaT)
+		self.whiteNoise += np.random.normal(0, deltaT)
 	def isGrowable(self):
 		return self.growable
 	def setGrowable(self, value):
